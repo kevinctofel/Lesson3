b7b0d692f6c253092e2a070b0d1c9c27d0d6be91
Kevin C. Tofel <kevin@kctofel.com>
2022-04-27 15:45:05 -0400

    test file for working on problems

---
+++ test.js
@@ -0,0 +1,6 @@
+let munstersDescription = "the Munsters are CREEPY and Spooky.";
+// => The munsters are creepy and spooky.
+
+munsters = munstersDescription.charAt(0).toUpperCase() + munstersDescription.slice(1).toLowerCase();
+
+console.log(munsters);
