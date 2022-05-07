e3985f5c291cf5b02efca969debdf8dcd8208926
Kevin C. Tofel <kevin@kctofel.com>
2022-05-07 17:00:48 -0400

    Completed Hard1

--- test.js
+++ test.js
@@ -1,12 +1,24 @@
-// 
+function isAnIpNumber(str) {
+    if (/^\d+$/.test(str)) {
+        let number = Number(str);
+        return number >= 0 && number <= 255;
+    }
 
-function foo(param = "no") {
-    return "yes";
-  }
-  
-  function bar(param = "no") {
-    return param === "no" ? "yes" : "no";
-  }
+    return false;
+}
 
-  console.log(bar(foo()));
-  
+function isDotSeparatedIpAddress(inputString) {
+    let dotSeparatedWords = inputString.split(".");
+    if (dotSeparatedWords.length === 4) {
+        while (dotSeparatedWords.length > 0) {
+            let word = dotSeparatedWords.pop();
+            if (!isAnIpNumber(word)) {
+                return true        // add
+            } else return false; // add
+        }
+    } else {
+        return ("IP addresses must have four dot compontnents") // add
+    }
+}
+
+console.log(isDotSeparatedIpAddress('10.4.5.11'));
